7d0355ea9e3d9a58d9de1e2ebec060eadb7d9fb9
MDR57 <riomardiego@gmail.com>
2025-03-28 11:19:09 +0100

    Create Conexion.java

---
+++ src/Conexion.java
@@ -0,0 +1,16 @@
+public class Conexion {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://127.0.0.1:3306/bdnicotek";
+        String user = "admin";
+        String password = "1234";
+
+        try {
+            // Conexión a la base de datos
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexión exitosa a la base de datos.");
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
