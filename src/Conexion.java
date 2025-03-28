ad8570ea08d29787b309850dc4fccdccdbbd0111
MDR57 <riomardiego@gmail.com>
2025-03-28 12:18:38 +0100

    Update Conexion.java

--- src/Conexion.java
+++ src/Conexion.java
@@ -1,6 +1,6 @@
 public class Conexion {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://127.0.0.1:3306/bdnicotek";
+        String url = "jdbc:mysql://127.0.0.1:3306/bdbiblioteca";
         String user = "admin";
         String password = "1234";
 
