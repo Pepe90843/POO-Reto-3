30f4db9f1d1ff3ca9fc9cf37786b6a91ecf8e04f
MDR57 <riomardiego@gmail.com>
2025-04-01 12:26:20 +0200

    Update Conexion.java

--- src/Conexion.java
+++ src/Conexion.java
@@ -2,27 +2,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
-
 public class Conexion{
     public static void main(String[] args) {
-        String url = "jdbc:mysql://192.168.56.1:3306/bdbiblioteca";
+        String url = "jdbc:mysql://25.7.162.154:3306/bdbiblioteca";
         String user = "admin";
         String password = "1234";
-        Connection conn=null;
 
         try {
             // Conexión a la base de datos
-            conn = BaseDeDatos.ConexionBaseDeDatos(url, user, password);
-            if (conn==null) {
-                BaseDeDatos.mensa("No se pudo establecer la conexión");
-                return;
-            }
+            Connection conn = DriverManager.getConnection(url, user, password);
             System.out.println("Conexión exitosa a la base de datos.");
+            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-        BaseDeDatos.cerrarConexion(conn);
     }
 }
