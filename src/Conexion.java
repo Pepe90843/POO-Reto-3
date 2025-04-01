a5e3837b3728864784fdff84da98e7050ff8cfdb
Asier <asier.uriarte06@somo.eus>
2025-04-01 12:42:11 +0200

    Update Conexion.java

--- src/Conexion.java
+++ src/Conexion.java
@@ -2,15 +2,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
-
 public class Conexion{
     public static void main(String[] args) {
         String url = "jdbc:mysql://192.168.56.1:3306/bdbiblioteca";
         String user = "admin";
         String password = "1234";
         Connection conn=null;
-
+        System.out.println("*****************************************************************************************");
+        System.out.println("***                             BIBLIOTECA MUSKIZ                                    ****");
+        System.out.println("*****************************************************************************************");
         try {
             // Conexión a la base de datos
             conn = BaseDeDatos.ConexionBaseDeDatos(url, user, password);
