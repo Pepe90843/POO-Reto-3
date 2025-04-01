b2fe956df1aa5f206750306039ac5ae24821d1cb
Asier <asier.uriarte06@somo.eus>
2025-04-01 12:50:59 +0200

    Restaurado todo

---
+++ src/Conexion.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+public class Conexion{
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://192.168.56.1:3306/bdbiblioteca";
+        String user = "admin";
+        String password = "1234";
+        Connection conn=null;
+
+        try {
+            // Conexión a la base de datos
+            conn = BaseDeDatos.ConexionBaseDeDatos(url, user, password);
+            if (conn==null) {
+                BaseDeDatos.mensa("No se pudo establecer la conexión");
+                return;
+            }
+            System.out.println("Conexión exitosa a la base de datos.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        BaseDeDatos.cerrarConexion(conn);
+    }
+}
