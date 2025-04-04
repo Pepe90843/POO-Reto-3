dc91473e316082c1e052d55f14146f795b075fbd
Asier <asier.uriarte06@somo.eus>
2025-04-04 08:32:45 +0200

    Arreglo de la presentación actual del programa

--- src/Conexion.java
+++ src/Conexion.java
@@ -1,5 +1,6 @@
 java.lang.ClassNotFoundException: Conexion;
 import java.sql.Connection;
+import java.util.Scanner;
 
 public class Conexion {
     public static void main(String[] args) {
@@ -7,19 +8,35 @@ public class Conexion {
         String user = "admin";
         String password = "1234";
         Connection conn = null;
+        Scanner teclado=new Scanner(System.in);
 
         System.out.println("*********************************************************************");
         System.out.println("***/                    BIBLIOTECA MUSKIZ                        /***");
         System.out.println("*********************************************************************\n");
+        System.out.println("       1-Obtener conexión                 4-Cerrar conexión");
 
-        // Conexión a la base de datos
-        conn = BaseDeDatos.ConexionBaseDeDatos(url, user, password);
-        if (conn == null) {
-            BaseDeDatos.mensa("No se pudo establecer la conexión");
-            return;
+
+        int eleccion = teclado.nextInt();
+
+        switch (eleccion) {
+            case 1:
+                // Conexión a la base de datos
+                conn = BaseDeDatos.ConexionBaseDeDatos(url, user, password);
+                if (conn == null) {
+                    BaseDeDatos.mensa("No se pudo establecer la conexión");
+                    return;
+                }
+                System.out.println("Conexión exitosa a la base de datos.");
+                break;
+        
+
+            case 4:
+                BaseDeDatos.cerrarConexion(conn);
+                BaseDeDatos.mensa("Conexión cerrada exitosamente!");
+                break;
+            default:
+                BaseDeDatos.mensa("Elige una de las opciones que se dan.");
+                break;
         }
-        System.out.println("Conexión exitosa a la base de datos.");
-
-        BaseDeDatos.cerrarConexion(conn);
     }
 }
